72224181f2d2c8807c75a9201744e6e6d7dc3e35
ismailkad <64145136+ismailkad@users.noreply.github.com>
2020-04-22 18:55:23 +0200

    Update tp3.h

--- tp3.h
+++ tp3.h
@@ -1,1 +1,19 @@
+typedef struct element {
+    int colonne;
+    int valeur;
+    struct element* suivant;
+} element;
 
+typedef element* liste_lignes;
+
+typedef struct matrice_creuse {
+    liste_lignes* tableauLignes;
+    int Nlignes;
+    int Ncolonnes;
+} matrice_creuse;
+
+void remplirMat(matrice_creuse *m, int N, int M);
+void afficherMat(matrice_creuse m);
+void addMat(matrice_creuse m1, matrice_creuse m2);
+int getValue(matrice_creuse m, int i, int j);
+void putValue(matrice_creuse m, int i, int j, int val);

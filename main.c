e918eae0dd8d4a3f4363bee3875f53d6453520d3
ismailkad <64145136+ismailkad@users.noreply.github.com>
2020-04-24 01:57:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,66 +4,61 @@
 
 int main() {
     
-    int fin, nb_m;
-    int i=0, j;
+    int fin;
+    matrice_creuse m1, m2;
     
-    all_matrice *matrice;
-    
-    printf("Entrez le nombre de matrice souhaité: ");
-    scanf("%d", &nb_m);
-    
-    matrice->nb_matrice = nb_m;
-    matrice->tab_mat = malloc(nb_m * sizeof(matrice_creuse)); //création d'un tableau dynamique qui stockera l'adresse de nos matrices
-    
+    int i = 0; //drapeau qui permettra de déterminer si une matrice ou deux matrices ont été enregistré
     fin = 0; //drapeau qui permettra de déterminer la fin de notre programme
     
-    
+
     while(!fin) {
         
-        int c, d, k;
-        int N, M, val; //i sera incrementé à chaque fois qu'une matrice sera créee
-        
-        matrice_creuse m;
+        int c=0, a;
+        int N, M, val;
         
         /* affichage menu */
-        printf("1. Remplir une matrice creuse\n"
+        printf("Menu:\n"
+               "1. Remplir une matrice creuse\n"
                "2. Afficher une matrice creuse\n"
                "3. Donner la valeur d'un élément d'une matrice creuse\n"
                "4. Affecter une valeur à un élément d'une matrice creuse\n"
                "5. Additionner deux matrices creuses\n"
                "6. Calculer le gain en espace en utilisant cette représentation pour une matrice donnée\n"
                "7. Quitter\n"
+               "Votre choix: "
                );
         
-        c = getchar();
-        
-        /* suppression des caracteres dans stdin */
-        if(c != '\n' && c != EOF) {
-            int d;
-            while((d = getchar()) != '\n' && d != EOF);
-        }
+        scanf("%d",&c);
         
         switch(c) {
                 
-            case '1':
+            case 1:
                 
-                printf("Nb de lignes de la matrice: ");
-                scanf("%d", &N);
-                
-                printf("Nb de colonnes de la matrice: ");
-                scanf("%d", &M);
-                
-                remplirMat(&m, N, M);
-                
-                matrice->tab_mat[i] = m;
-                i++;
-                
-                printf("La matrice a bien été crée.\n");
+                if (i > 1) {
+                    printf("Deux matrices ont déjà été enregistrées. \n");
+                    break;
+                }
+                else {
+                    printf("Nb de lignes de la matrice: ");
+                    scanf("%d", &N);
+                    
+                    printf("Nb de colonnes de la matrice: ");
+                    scanf("%d", &M);
+                    
+                    if (i == 0) {
+                        remplirMat(&m1, N, M);
+                        i = 1;
+                    }
+                    else if (i == 1) {
+                        remplirMat(&m2, N, M);
+                        i += 1;
+                    }
             
+                    printf("La matrice a bien été créee.\n");
+                    break;
+                }
                 
-                break;
-                
-            case '2':
+            case 2:
                 
                 if (i == 0) {
                     printf("Il n'y'a aucune matrice stockée");
@@ -71,24 +66,21 @@ int main() {
                 }
                 
                 else {
-                    printf("Choisir la matrice a afficher: \n");
-                    for (j=0; j<i; j++) {
-                        printf("%d. Matrice n°%d.\n", j+1, j+1);
-                    }
+                    printf("Choisir la matrice a afficher: \n 1: Matrice N°1\n 2: Matrice N°2\n "
+                           "Votre choix: ");
+                    scanf("%d", &a);
                     
-                    d = getchar();
-                    
-                    if(d != '\n' && d != EOF) {
-                        int e;
-                        while((e = getchar()) != '\n' && e != EOF);
-                    }
-                    
-                    afficherMat(matrice->tab_mat[d]);
+                    if ((a == 1) && (i>0))
+                        afficherMat(m1);
+                    else if ((a == 2) && (i>1))
+                        afficherMat(m2);
+                    else
+                        printf("Mauvaise saisie\n");
                     
                     break;
                 }
                 
-            case '3':
+            case 3:
                 
                 if (i == 0) {
                     printf("Il n'y'a aucune matrice stockée");
@@ -96,27 +88,30 @@ int main() {
                 }
                 else {
                     
-                    printf("Choisir la matrice de la valeur a afficher: \n");
-                    for (j=0; j<i; j++) {
-                        printf("Matrice n°%d.\t", j+1);
+                    printf("Choisir la matrice: \n 1: Matrice N°1\n 2: Matrice N°2\n "
+                           "Votre choix: ");
+                    scanf("%d", &a);
+                    
+                    if ((a > 2) || (a < 0)) {
+                        printf("Mauvaise saisie");
+                        break;
                     }
-                    
-                    d = getchar();
-                    
-                    if(d != '\n' && d != EOF) {
-                        int e;
-                        while((e = getchar()) != '\n' && e != EOF);
+                    else {
+                        printf("Numéro de la ligne de l'élément a afficher: ");
+                        scanf("%d", &N);
+                        
+                        printf("Numéro de la colonne de l'élément a afficher: ");
+                        scanf("%d", &M);
+                        
+                        if (a == 1)
+                            printf("%d\n",getValue(m1, N, M));
+                        else if (a == 2)
+                            printf("%d\n",getValue(m2, N, M));
+                        break;
                     }
-                    printf("Numéro de la ligne de l'élément a afficher: ");
-                    scanf("%d", &N);
-                    
-                    printf("Numéro de la colonne de l'élément a afficher: ");
-                    scanf("%d", &M);
-                    
-                    getValue(matrice->tab_mat[d], N, M);
                 }
                 
-            case '4':
+            case 4:
                     
                 if (i == 0) {
                     printf("Il n'y'a aucune matrice stockée");
@@ -124,73 +119,75 @@ int main() {
                 }
                 else {
                     
-                    printf("Choisir la matrice a modifier: \n");
-                    for (j=0; j<i; j++) {
-                        printf("Matrice n°%d.\t", j+1);
+                    printf("Choisir la matrice: \n 1. Matrice N°1\n 2. Matrice N°2\n "
+                           "Votre choix: ");
+                    scanf("%d", &a);
+                    
+                    if ( (a<0) || (a>2) ) {
+                        printf("Mauvaise saisie\n");
+                        break;
                     }
                     
-                    d = getchar();
-                    
-                    if(d != '\n' && d != EOF) {
-                        int e;
-                        while((e = getchar()) != '\n' && e != EOF);
+                    else {
+                        printf("Numéro de la ligne de l'élément a modifier: ");
+                        scanf("%d", &N);
+                        
+                        printf("Numéro de la colonne de l'élément a modifier: ");
+                        scanf("%d", &M);
+                        
+                        printf("Valeur de l'élément a affecter: ");
+                        scanf("%d", &val);
+                        
+                        if (a == 1) {
+                            putValue(m1, N, M, val);
+                            printf("L'élément a bien été modifié. \n");
+                        }
+                        else if (a == 2) {
+                            putValue(m1, N, M, val);
+                            printf("L'élément a bien été modifié. \n");
+                        }
+                        
+                        break;
                     }
+                }
                     
-                    printf("Numéro de la ligne de l'élément a modifier: ");
-                    scanf("%d", &N);
-                    
-                    printf("Numéro de la colonne de l'élément a modifier: ");
-                    scanf("%d", &M);
-                    
-                    printf("Valeur de l'élément a affecter: ");
-                    scanf("%d", &val);
-                    
-                    putValue(matrice->tab_mat[d], N, M, val);
-                    
-                    printf("L'élément a bien été modifié. \n");
-                    
+            case 5:
+                
+                addMat(m1, m2);
+                afficherMat(m1);
+    
+                break;
+                
+            case 6:
+                
+                if (i == 0) {
+                    printf("Il n'y'a aucune matrice stockée");
                     break;
                 }
+                else {
+                    printf("Choisir la matrice: \n 1. Matrice N°1\n 2. Matrice N°2\n "
+                           "Votre choix: ");
+                    scanf("%d", &a);
                     
-            case '5':
-                
-                printf("Choisir les deux matrices a afficher: \n");
-                for (j=0; j<i; j++) {
-                    printf("Matrice n°%d.\t", j+1);
+                    if ( (a<0) || (a>2) ) {
+                        printf("Mauvaise saisie\n");
+                        break;
+                    }
+                    
+                    else {
+                        
+                        if (a == 1) {
+                            printf("Le nombre d'octets gagnés pour la matrice : %d\n",nombreOctetsGagnes(m1));
+                        }
+                        else if (a == 2) {
+                            printf("Le nombre d'octets gagnés pour la matrice : %d\n",nombreOctetsGagnes(m2));
+                        }
+                        
+                        break;
+                    }
                 }
-                
-                d = getchar();
-                k = getchar();
-                
-                if(d != '\n' && d != EOF) {
-                    int e;
-                    while((e = getchar()) != '\n' && e != EOF);
-                }
-                
-                addMat(matrice->tab_mat[d], matrice->tab_mat[k]);
-                afficherMat(matrice->tab_mat[d]);
-                
-                break;
-                
-            case '6':
-                
-                printf("Choisir la matrice: \n");
-                for (j=0; j<i; j++) {
-                    printf("Matrice n°%d.\t", j+1);
-                }
-                
-                d = getchar();
-                
-                if(d != '\n' && d != EOF) {
-                    int e;
-                    while((e = getchar()) != '\n' && e != EOF);
-                }
-                
-                nombreOctetsGagnes(matrice->tab_mat[d]);
-                
-                break;
-                
-            case '7':
+
+            case 7:
                 fin = 1;
                 break;
                 
@@ -201,5 +198,4 @@ int main() {
     
     return 0;
 }
-
 
